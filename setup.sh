1f6cafe4820cd77d1334ce903c1b3eff3a8fb19d
WideTracked <harrismajeed2004@gmail.com>
2023-01-09 23:58:41 +0800

    make bash script simpler

--- setup.sh
+++ setup.sh
@@ -1,11 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
-#install necessary packages to sudo enviroment
 sudo pip3 install -r requirements.txt
-#make script executable
-chmod +x scan-daemon.py
-#add script to crontab
-crontab -l > temp/crontab-temp
-echo "0 * * * * /usr/bin/python3 /home/void/Documents/fyp-fim-thigendra/scan-daemon.py" >> temp/crontab-temp
-crontab temp/crontab-temp
-rm temp/crontab-temp
+crontab -l | { cat; echo "0 * * * * /usr/bin/python3 /home/void/Documents/fyp-fim-thigendra/scan-daemon.py"; } | crontab -
