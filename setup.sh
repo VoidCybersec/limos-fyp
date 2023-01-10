421d67520f0d73f603984d9a53a40d7b1caa5244
WideTracked <30569190+WideTracked@users.noreply.github.com>
2023-01-10 08:55:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo pip3 install -r requirements.txt
-crontab -l | { cat; echo "0 * * * * /usr/bin/python3 /home/void/Documents/fyp-fim-thigendra/scan-daemon.py"; } | crontab -
+crontab -l | { cat; echo "0 * * * * /usr/bin/python3 $(pwd)/scan-daemon.py"; } | crontab -
