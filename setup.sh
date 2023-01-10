6a0ad107d297d0005d10ed2a3a9ac74359d7fee5
WideTracked <30569190+WideTracked@users.noreply.github.com>
2023-01-10 16:04:24 +0800

    add warning

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# ONLY RUN THIS SCRIPT FROM ITS ORIGINAL DIRECTORY
 
 sudo pip3 install -r requirements.txt
 crontab -l | { cat; echo "0 * * * * /usr/bin/python3 $(pwd)/scan-daemon.py"; } | crontab -
